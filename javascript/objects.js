476e8539b741085cff061627b68670d8274a2809
William Gabriel Braga <70717232+Yckson@users.noreply.github.com>
2022-12-30 16:53:18 -0300

    commmit final

--- javascript/objects.js
+++ javascript/objects.js
@@ -40,6 +40,8 @@ const TelaDeLogin = {
         ["FW9F-ivAypGuVtY8EpJSXA", "DTkzvXbkaLqgeLvTS9hSlg"], //bruno - energia | bruno - o caos lhe consome
 
         ["xXGrCndov5-Eqn_XN7Z3Ag", "lOP2go2Colm-e4xn1186Iw"], //akin - conhecimento | akin - saber tudo...
+
+        ["1234", "1234567"]
     ],
 
     start: function (){
@@ -92,12 +94,16 @@ const TelaDeLogin = {
                 hideContent(this.error);
                 showContent(this.progressBar);
                 playAudioOver('startingSYS');
+                hideContent(this.buttonStart);
                 for (let c = 0; c < 100; c++){
                     loadingBar.style.width = `${c}%`;
                     await sleep(50);
                 }
+                await sleep(1000);
                 this.currentLoggedUser[0] = document.querySelector(`input#${this.user}`).value;
-                this.currentLoggedUser[1] = document.querySelector(`input#${this.pw}`).value;
+                this.currentLoggedUser[1] = document.querySelector(`input#${this.password}`).value;
+                hideContent(this.element);
+                TelaDoVideo.start();
 
             }
             else{
@@ -106,10 +112,64 @@ const TelaDeLogin = {
             }
         }
         else{
-            errorSpan.innerHTML = "Usuário Inexistente!";
+            errorSpan.innerHTML = "Usuário Incorreto!";
             showContent(this.error);
         }
     }
 
 
+}
+
+const TelaDoVideo = {
+    element: 'view03',
+    video: 'video01',
+
+    start: function (){
+        showContent(this.element);
+        let video = document.querySelector(`video#${this.video}`);
+        video.play();
+        this.removeVideo();
+    },
+
+    removeVideo: async function (){
+        await sleep(13000);
+        hideContent(this.element);
+        sysOutput.start()
+    }
+}
+
+const sysOutput = {
+    element: 'view04',
+    key1: null,
+    key2: null,
+    key3: null,
+
+    start: function(){
+        this.key1 = document.createElement('img');
+        this.key1.src = 'media/img/key1.png';
+
+        this.key2 = document.createElement('span');
+        this.key2.id = 'textoEnergia';
+
+        this.key3 = document.createElement('img');
+        this.key3.src = 'media/img/key3.jpg';
+
+        let view = document.querySelector(`div#${this.element}`);
+
+        showContent(this.element);
+
+        if (TelaDeLogin.currentLoggedUser[0] == TelaDeLogin.list_of_users[0][0]){
+            view.appendChild(this.key1);
+        }
+        else if (TelaDeLogin.currentLoggedUser[0] == TelaDeLogin.list_of_users[1][0]){
+            view.appendChild(this.key2);
+            letterByLetter('textoEnergia', "Existe uma lenda de uma dimensão alternativa, um espaço que poderia ser cessado pelo puro acaso. Uma possibilidade minúscula, inacreditável, de que ao interagir com uma passagem, você poderia acessar outro lugar que não pretendia. Como se em todas as vezes que as portas do elevador se abrem, ou quando você se prepara para sair de casa, ou até mesmo quando abre o micro-ondas para pegar comida, você está jogando na loteria da probabilidade anárquica", 50);
+        }
+        else if (TelaDeLogin.currentLoggedUser[0] == TelaDeLogin.list_of_users[2][0]){
+            view.appendChild(this.key3);
+        }
+        else if (TelaDeLogin.currentLoggedUser[0] == TelaDeLogin.list_of_users[3][0]){
+            playAudio('mamae');
+        }
+    }
 }
